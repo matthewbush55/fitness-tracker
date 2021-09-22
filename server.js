35b125fca1f49543af1df9700392557d0b853520
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-09-21 18:58:58 -0600

    Added models

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const routes = require("./controllers");
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
 
