9b599e2f5742808ce77c1dd05eb6ab614d2d714c
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-09-21 18:37:24 -0600

    Initial commit

---
+++ public/models/workout.js
@@ -0,0 +1,33 @@
+// const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const BookSchema = new Schema({
+//   author: String,
+//   title: String
+// });
+
+// const Book = mongoose.model("Book", BookSchema);
+
+// module.exports = Book;
+
+// const mongoose = require("mongoose");
+
+// const Schema = mongoose.Schema;
+
+// const LibrarySchema = new Schema({
+//   name: {
+//     type: String,
+//     unique: true
+//   },
+//   books: [
+//     {
+//       type: Schema.Types.ObjectId,
+//       ref: "Book"
+//     }
+//   ]
+// });
+
+// const Library = mongoose.model("Library", LibrarySchema);
+
+// module.exports = Library;
